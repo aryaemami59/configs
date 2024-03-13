b9c2f877c3cb661ca686273f4f1c7e0c321167ac
Arya Emami <aryaemami59@yahoo.com>
2024-03-13 01:55:31 -0500

    Fix ESLint config for root monorepo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,4 @@
 import { createESLintConfig, flatESLintConfig } from '@arya/eslint-config'
 
-// console.log(flatESLintConfig.at(-1))
 
-export default createESLintConfig([
-  {
-    // ...flatESLintConfig.at(-1),
-    languageOptions: {
-      ...flatESLintConfig.at(-1)?.languageOptions,
-      parserOptions: {
-        // ecmaVersion: 'latest',
-        project: ['./packages/*/tsconfig.json'],
-        // tsconfigRootDir: './packages/*',
-      },
-    },
-  },
-])
+export default createESLintConfig()
