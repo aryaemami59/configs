e243a7933443c9a68672cfd3bc0cedf9d216310a
Arya Emami <aryaemami59@yahoo.com>
2024-04-22 22:14:50 -0500

    Fix unit tests for `prettier-config-example`

--- examples/prettier/prettier-ts-file.test.ts
+++ examples/prettier/prettier-ts-file.test.ts
@@ -2,60 +2,57 @@ import { exec as _exec } from 'node:child_process'
 import fs from 'node:fs/promises'
 import path from 'node:path'
 import { promisify } from 'node:util'
-import { afterAll, beforeEach, describe } from 'vitest'
 
-const exec = promisify(_exec)
+export const exec = promisify(_exec)
+
+export const cli = `prettier -c --ignore-path null`
 
 interface LocalTestContext {
   tempDir: string
   filePath: string
 }
 
-const tempDir = await fs.mkdtemp('.temp')
+describe<LocalTestContext>('TS file', async test => {
+  const tempDir = await fs.mkdtemp('.temp-ts')
 
-const fileContent =
-  'const someFunction: (...args: any[]) => any = (arg) => arg\n'
+  const fileContent =
+    'const someFunction: (...args: any[]) => any = (arg) => arg\n'
 
-describe.concurrent<LocalTestContext>('TS file', test => {
+  const filePath = path.join(tempDir, 'test.ts')
+
+  beforeAll(async () => {
+    await fs.writeFile(filePath, fileContent, 'utf-8')
+
+    return async () => {
+      await fs.rm(tempDir, { recursive: true, force: true })
+    }
+  })
+
   beforeEach<LocalTestContext>(async context => {
     context.tempDir = tempDir
 
-    context.filePath = path.join(context.tempDir, 'test.ts')
-  })
-
-  afterAll(async () => {
-    await fs.rm(tempDir, { recursive: true, force: true })
+    context.filePath = filePath
   })
 
   test('no config specified', async ({ expect, filePath, tempDir }) => {
-    await fs.writeFile(filePath, fileContent, 'utf-8')
-
-    await expect(() => exec(`prettier -c ${filePath}`)).rejects.toThrowError(
-      tempDir,
-    )
+    await expect(() => exec(`${cli} ${filePath}`)).rejects.toThrowError(tempDir)
   })
 
   test('prettier.config.js', async ({ expect, filePath, tempDir, task }) => {
-    await fs.writeFile(filePath, fileContent, 'utf-8')
-
     await expect(() =>
-      exec(`prettier -c ${filePath} --config ${task.name}`),
+      exec(`${cli} ${filePath} --config ${task.name}`),
     ).rejects.toThrowError(tempDir)
   })
 
   test('prettier.config.cjs', async ({ expect, filePath, tempDir, task }) => {
-    await fs.writeFile(filePath, fileContent, 'utf-8')
-
     await expect(() =>
-      exec(`prettier -c ${filePath} --config ${task.name}`),
+      exec(`${cli} ${filePath} --config ${task.name}`),
     ).rejects.toThrowError(tempDir)
   })
 
   test('prettier.config.mjs', async ({ expect, filePath, tempDir, task }) => {
-    await fs.writeFile(filePath, fileContent, 'utf-8')
-
     await expect(() =>
-      exec(`prettier -c ${filePath} --config ${task.name}`),
+      exec(`${cli} ${filePath} --config ${task.name}`),
     ).rejects.toThrowError(tempDir)
   })
 })
