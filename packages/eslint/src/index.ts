defea8c38fb482d69b9329f06e6a108970c64028
Arya Emami <aryaemami59@yahoo.com>
2024-09-22 10:53:24 -0500

    Add `@typescript-eslint/utils`

--- packages/eslint/src/index.ts
+++ packages/eslint/src/index.ts
@@ -1,4 +1,5 @@
 import js from '@eslint/js'
+import type { TSESLint } from '@typescript-eslint/utils'
 import prettierConfig from 'eslint-config-prettier'
 import globals from 'globals'
 import type { ConfigWithExtends } from 'typescript-eslint'
@@ -72,93 +73,94 @@ export const vitestGlobals = {
  * @since 0.0.3
  * @public
  */
-export const flatESLintConfig = /* @__PURE__ */ config(
-  // `ignores` must be first.
-  // config with just `ignores` is the replacement for `.eslintignore`
-  {
-    name: '@aryaemami59/global-ignores',
-    ignores: [
-      '**/dist/',
-      '**/.yalc/',
-      '**/build/',
-      '**/lib/',
-      '**/temp/',
-      '**/.yarn/',
-    ],
-  },
-  { name: '@aryaemami59/javascript', ...js.configs.recommended },
-  ...configs.recommended,
-  ...configs.stylistic,
-  { name: '@aryaemami59/prettier-config', ...prettierConfig },
-  {
-    name: '@aryaemami59/main',
-    languageOptions: {
-      globals: {
-        ...vitestGlobals,
-        ...browser,
-        ...node,
-        ...nodeBuiltin,
-      },
-      parser,
-      parserOptions: {
-        projectService: {
-          allowDefaultProject: ['.*.js', '.*.mjs', '.*.cjs'],
-          defaultProject: './tsconfig.json',
-        },
-        ecmaVersion: 'latest',
-      },
+export const flatESLintConfig: TSESLint.FlatConfig.Config[] =
+  /* @__PURE__ */ config(
+    // `ignores` must be first.
+    // config with just `ignores` is the replacement for `.eslintignore`
+    {
+      name: '@aryaemami59/global-ignores',
+      ignores: [
+        '**/dist/',
+        '**/.yalc/',
+        '**/build/',
+        '**/lib/',
+        '**/temp/',
+        '**/.yarn/',
+      ],
     },
-    rules: {
-      'no-undef': [0],
-      '@typescript-eslint/consistent-type-imports': [
-        2,
-        { fixStyle: 'separate-type-imports', disallowTypeAnnotations: false },
-      ],
-      '@typescript-eslint/consistent-type-exports': [2],
-      '@typescript-eslint/no-unused-vars': [0],
-      '@typescript-eslint/no-explicit-any': [0],
-      '@typescript-eslint/no-empty-object-type': [
-        2,
-        { allowInterfaces: 'with-single-extends' },
-      ],
-      '@typescript-eslint/no-restricted-types': [
-        2,
-        {
-          types: {
-            '{}': {
-              suggest: ['AnyNonNullishValue', 'EmptyObject', 'AnyObject'],
+    { name: '@aryaemami59/javascript', ...js.configs.recommended },
+    ...configs.recommended,
+    ...configs.stylistic,
+    { name: '@aryaemami59/prettier-config', ...prettierConfig },
+    {
+      name: '@aryaemami59/main',
+      languageOptions: {
+        globals: {
+          ...vitestGlobals,
+          ...browser,
+          ...node,
+          ...nodeBuiltin,
+        },
+        parser,
+        parserOptions: {
+          projectService: {
+            allowDefaultProject: ['.*.js', '.*.mjs', '.*.cjs'],
+            defaultProject: './tsconfig.json',
+          },
+          ecmaVersion: 'latest',
+        },
+      },
+      rules: {
+        'no-undef': [0],
+        '@typescript-eslint/consistent-type-imports': [
+          2,
+          { fixStyle: 'separate-type-imports', disallowTypeAnnotations: false },
+        ],
+        '@typescript-eslint/consistent-type-exports': [2],
+        '@typescript-eslint/no-unused-vars': [0],
+        '@typescript-eslint/no-explicit-any': [0],
+        '@typescript-eslint/no-empty-object-type': [
+          2,
+          { allowInterfaces: 'with-single-extends' },
+        ],
+        '@typescript-eslint/no-restricted-types': [
+          2,
+          {
+            types: {
+              '{}': {
+                suggest: ['AnyNonNullishValue', 'EmptyObject', 'AnyObject'],
+              },
             },
           },
-        },
-      ],
-      '@typescript-eslint/no-namespace': [
-        2,
-        { allowDeclarations: true, allowDefinitionFiles: true },
-      ],
-      '@typescript-eslint/ban-ts-comment': [0],
-      '@typescript-eslint/consistent-type-definitions': [0, 'type'],
-      'sort-imports': [
-        2,
-        {
-          ignoreCase: false,
-          ignoreDeclarationSort: true,
-          ignoreMemberSort: false,
-          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-          allowSeparatedGroups: true,
-        },
-      ],
+        ],
+        '@typescript-eslint/no-namespace': [
+          2,
+          { allowDeclarations: true, allowDefinitionFiles: true },
+        ],
+        '@typescript-eslint/ban-ts-comment': [0],
+        '@typescript-eslint/consistent-type-definitions': [0, 'type'],
+        'sort-imports': [
+          2,
+          {
+            ignoreCase: false,
+            ignoreDeclarationSort: true,
+            ignoreMemberSort: false,
+            memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+            allowSeparatedGroups: true,
+          },
+        ],
+      },
+      linterOptions: { reportUnusedDisableDirectives: 2 },
     },
-    linterOptions: { reportUnusedDisableDirectives: 2 },
-  },
-  {
-    name: '@aryaemami59/commonjs',
-    files: ['**/*.c[jt]s'],
-    languageOptions: { sourceType: 'commonjs' },
-    rules: {
-      '@typescript-eslint/no-require-imports': [0],
+    {
+      name: '@aryaemami59/commonjs',
+      files: ['**/*.c[jt]s'],
+      languageOptions: { sourceType: 'commonjs' },
+      rules: {
+        '@typescript-eslint/no-require-imports': [0],
+      },
     },
-  },
-)
+  )
 
 /**
  * A function that returns {@linkcode flatESLintConfig}
@@ -246,4 +248,5 @@ export const flatESLintConfig = /* @__PURE__ */ config(
  */
 export const createESLintConfig = (
   additionalOverrides: ConfigWithExtends[] = [],
-) => /* @__PURE__ */ config(...flatESLintConfig, ...additionalOverrides)
+): TSESLint.FlatConfig.Config[] =>
+  /* @__PURE__ */ config(...flatESLintConfig, ...additionalOverrides)
