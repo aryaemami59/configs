23ab8824a68dd8ac75844d78a81eabcf622ce11f
Arya Emami <aryaemami59@yahoo.com>
2025-04-23 12:32:11 -0500

    Add JSDocs for `vitest` config

--- packages/vitest/src/defaults.ts
+++ packages/vitest/src/defaults.ts
@@ -8,22 +8,55 @@ import { plugins } from './plugins.js'
  * @public
  */
 export const vitestProjectDefaults = {
+  /**
+   * @default { 'import.meta.vitest': 'undefined' }
+   */
   define: {
+    /**
+     * @default 'undefined'
+     */
     'import.meta.vitest': 'undefined',
   },
 
+  /**
+   * @default [tsconfigPaths({ projects: ['./tsconfig.json'], configNames: ['tsconfig.json'] })]
+   */
   plugins,
 
   test: {
+    /**
+     * @default true
+     */
     clearMocks: true,
+
+    /**
+     * @default true
+     */
     globals: true,
 
+    /**
+     * @default { enabled: true, tsconfig: './tsconfig.json' }
+     */
     typecheck: {
+      /**
+       * @default true
+       */
       enabled: true,
+
+      /**
+       * @default './tsconfig.json'
+       */
       tsconfig: './tsconfig.json',
     },
 
+    /**
+     * @default true
+     */
     unstubEnvs: true,
+
+    /**
+     * @default true
+     */
     unstubGlobals: true,
   },
 } as const satisfies UserWorkspaceConfig
@@ -40,15 +73,31 @@ export const vitestConfigDefaults = {
   test: {
     ...vitestProjectDefaults.test,
 
+    /**
+     * @default { extension: ['.ts', '.tsx', '.js', '.jsx', '.mts', '.mjs', '.cts', '.cjs'], include: ['src'] }
+     */
     coverage: {
+      /**
+       * @default ['.ts', '.tsx', '.js', '.jsx', '.mts', '.mjs', '.cts', '.cjs']
+       */
       extension: ['.ts', '.tsx', '.js', '.jsx', '.mts', '.mjs', '.cts', '.cjs'],
+
+      /**
+       * @default ['src']
+       */
       include: ['src'],
     },
 
+    /**
+     * @default process.env.GITHUB_ACTIONS ? [['verbose', { summary: false }], ['github-actions']] : [['verbose']]
+     */
     reporters: process.env.GITHUB_ACTIONS
       ? ([['verbose', { summary: false }], ['github-actions']] as const)
       : ([['verbose']] as const),
 
+    /**
+     * @default false
+     */
     watch: false,
   },
 } as const satisfies ViteUserConfig
