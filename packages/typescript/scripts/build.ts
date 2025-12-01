13d85d3b7549047d4adef77d3a2654dd6fa4c507
Arya Emami <aryaemami59@yahoo.com>
2025-12-01 02:51:36 -0600

    fix `tsconfig.json` for `@aryaemami59/tsconfig`

--- packages/typescript/scripts/build.ts
+++ packages/typescript/scripts/build.ts
@@ -2,7 +2,10 @@
 
 import * as fs from 'node:fs/promises'
 import * as path from 'node:path'
+import { format, resolveConfig } from 'prettier'
 import * as ts from 'typescript'
+import packageJson from '../package.json' with { type: 'json' }
+import type { Module, ModuleResolution, TsConfigJson } from './types.ts'
 
 const { ModuleResolutionKind, ModuleKind } = ts.server.protocol
 
@@ -12,10 +15,10 @@ type ModuleKindType = typeof ModuleKind
 
 const ROOT_DIRECTORY = path.join(import.meta.dirname, '..')
 
-// type DistributedOmit<
-//   ObjectType,
-//   KeyType extends keyof ObjectType,
-// > = ObjectType extends unknown ? Omit<ObjectType, KeyType> : never
+type DistributedOmit<
+  ObjectType,
+  KeyType extends keyof ObjectType,
+> = ObjectType extends unknown ? Omit<ObjectType, KeyType> : never
 
 /**
  * Any function with unknown arguments.
@@ -66,19 +69,10 @@ export type ExcludeStrict<
     : T,
 > = Exclude<T, U>
 
-// const moduleResolutionKinds = [
-//   ...new Set(
-//     Object.values(ModuleResolutionKind).filter(
-//       (moduleResolutionKind) =>
-//         moduleResolutionKind !== ModuleResolutionKind.Classic,
-//     ),
-//   ),
-// ]
-
 type KebabCase<
-  S extends string,
+  StringType extends string,
   FirstRun extends boolean = true,
-> = S extends `${infer FirstLetter}${infer Rest}`
+> = StringType extends `${infer FirstLetter}${infer Rest}`
   ? `${FirstLetter extends Lowercase<FirstLetter>
       ? FirstLetter extends `${number}`
         ? Rest extends `${number}`
@@ -88,128 +82,242 @@ type KebabCase<
       : FirstRun extends true
         ? ''
         : '-'}${Lowercase<FirstLetter>}${KebabCase<Rest, false>}`
-  : S
+  : StringType
 
-type ModuleResolutions = Simplify<
-  {
-    readonly [ModuleResolutionKey in ExcludeStrict<
-      keyof ModuleResolutionKindType,
-      'Classic' | 'NodeJs'
-    > as ModuleResolutionKindType[ModuleResolutionKey]]: {
-      readonly directory: ModuleResolutionKey extends 'Node'
-        ? 'base'
-        : KebabCase<ModuleResolutionKey>
-    }
-  } & {
-    readonly [ModuleResolutionKey in ExtractStrict<
-      ExcludeStrict<keyof ModuleResolutionKindType, 'Classic' | 'NodeJs'>,
-      'Node' | 'Node10' | 'Bundler'
-    > as ModuleResolutionKindType[ModuleResolutionKey]]: {
-      readonly modules: {
-        [K in ExtractStrict<
-          keyof ModuleKindType,
-          'ESNext'
-        > as ModuleKindType[K]]: ModuleKindType[K]
-      }
-    }
-  } & {
-    readonly [ModuleResolutionKind.Bundler]: {
-      readonly modules: {
-        readonly [K in ExtractStrict<
-          keyof ModuleKindType,
-          'ESNext' | 'Preserve'
-        > as ModuleKindType[K]]: ModuleKindType[K]
-      }
-    }
-  } & {
-    readonly [ModuleResolutionKind.Node16]: {
-      readonly modules: {
-        readonly [K in ExtractStrict<
-          keyof ModuleKindType,
-          'Node16'
-        > as ModuleKindType[K]]: ModuleKindType[K]
-      }
-    }
-  } & {
-    readonly [ModuleResolutionKind.NodeNext]: {
-      readonly modules: {
-        readonly [K in ExtractStrict<
-          keyof ModuleKindType,
-          'NodeNext'
-        > as ModuleKindType[K]]: ModuleKindType[K]
-      }
-    }
-  }
+type ExtractLowercase<StringType extends string> =
+  StringType extends Lowercase<StringType> ? StringType : never
+
+type ExtractCapitalized<StringType extends string> =
+  StringType extends Capitalize<StringType> ? StringType : never
+
+type LowerCaseModuleResolutionKinds = ExcludeStrict<
+  ExtractLowercase<ModuleResolution>,
+  'classic'
 >
 
-const moduleResolutions = {
-  [ModuleResolutionKind.Node]: {
-    directory: 'base',
-    modules: {
-      [ModuleKind.ESNext]: ModuleKind.ESNext,
-    },
-  },
-  [ModuleResolutionKind.Node10]: {
-    directory: 'node-10',
-    modules: {
-      [ModuleKind.ESNext]: ModuleKind.ESNext,
-    },
-  },
-  [ModuleResolutionKind.Node16]: {
-    directory: 'node-16',
-    modules: {
-      [ModuleKind.Node16]: ModuleKind.Node16,
-    },
-  },
-  [ModuleResolutionKind.NodeNext]: {
-    directory: 'node-next',
-    modules: {
-      [ModuleKind.NodeNext]: ModuleKind.NodeNext,
-    },
-  },
-  [ModuleResolutionKind.Bundler]: {
+type CapitalizedModuleResolutionKinds = ExcludeStrict<
+  ExtractCapitalized<ModuleResolution>,
+  'Classic'
+>
+
+type CapitalizedModuleKinds = ExcludeStrict<
+  ExtractCapitalized<Module>,
+  'AMD' | 'None' | 'System' | 'UMD'
+>
+
+type LowerCaseModuleKinds = ExcludeStrict<
+  ExtractLowercase<Module>,
+  'amd' | 'none' | 'system' | 'umd'
+>
+
+type CapitalizedToLowerCaseModuleResolutionKinds = {
+  readonly [K in CapitalizedModuleResolutionKinds]: Lowercase<K>
+}
+
+type LowerCaseToCapitalizedModuleResolutionKinds = {
+  readonly [K in CapitalizedModuleResolutionKinds as Lowercase<K>]: K
+}
+
+type PossibleModuleKinds = Simplify<{
+  readonly [K in CapitalizedModuleResolutionKinds]: {
+    readonly moduleResolution: Lowercase<K>
+    readonly module: Lowercase<K> extends 'bundler'
+      ? readonly ['esnext', 'preserve']
+      : Lowercase<K> extends 'node' | 'node10'
+        ? readonly ['esnext']
+        : Lowercase<K> extends 'node16'
+          ? readonly ['node16']
+          : Lowercase<K> extends 'nodenext'
+            ? readonly ['nodenext']
+            : readonly ['esnext']
+  }
+}>
+
+type BaseConfigs = {
+  readonly [K in CapitalizedModuleResolutionKinds as Lowercase<K>]: Simplify<{
+    readonly directory: KebabCase<K>
+    readonly module: PossibleModuleKinds[K]['module'][0]
+    readonly moduleResolution: Lowercase<K>
+    readonly subDirectory: [1] extends [
+      PossibleModuleKinds[K]['module']['length'],
+    ]
+      ? ''
+      : PossibleModuleKinds[K]['module'][0]
+  }>
+}
+
+const baseConfigs = {
+  bundler: {
     directory: 'bundler',
-    modules: {
-      [ModuleKind.ESNext]: ModuleKind.ESNext,
-      [ModuleKind.Preserve]: ModuleKind.Preserve,
-    },
+    module: 'esnext',
+    moduleResolution: 'bundler',
+    subDirectory: 'esnext',
   },
-} as const satisfies ModuleResolutions
+  node: {
+    directory: 'node',
+    module: 'esnext',
+    moduleResolution: 'node',
+    subDirectory: '',
+  },
+  node10: {
+    directory: 'node-10',
+    module: 'esnext',
+    moduleResolution: 'node10',
+    subDirectory: '',
+  },
+  node16: {
+    directory: 'node-16',
+    module: 'node16',
+    moduleResolution: 'node16',
+    subDirectory: '',
+  },
+  nodenext: {
+    directory: 'node-next',
+    module: 'nodenext',
+    moduleResolution: 'nodenext',
+    subDirectory: '',
+  },
+} as const satisfies BaseConfigs
 
-const directories = Object.entries(moduleResolutions).map(
-  ([, { directory }]) => directory,
-)
+type AdditionalConfigs = {
+  readonly [K in keyof PossibleModuleKinds as [1] extends [
+    PossibleModuleKinds[K]['module']['length'],
+  ]
+    ? never
+    : [undefined] extends [PossibleModuleKinds[K]['module']['1']]
+      ? never
+      : Lowercase<K>]: {
+    readonly directory: KebabCase<K>
+    readonly moduleResolution: PossibleModuleKinds[K]['moduleResolution']
+  } & {
+    [P in PossibleModuleKinds[K]['module'][ExtractStrict<
+      keyof PossibleModuleKinds[K]['module'],
+      number
+    >]]: {
+      readonly module: P
+      readonly subDirectory: P
+    }
+  }[ExcludeStrict<
+    PossibleModuleKinds[K]['module'][ExtractStrict<
+      keyof PossibleModuleKinds[K]['module'],
+      number
+    >],
+    PossibleModuleKinds[K]['module'][0]
+  >]
+}
 
-// const absoluteDirectories = directories.map((directory) =>
-//   path.join(ROOT_DIRECTORY, directory),
-// )
+const additionalConfigs = {
+  bundler: {
+    moduleResolution: 'bundler',
+    module: 'preserve',
+    directory: 'bundler',
+    subDirectory: 'preserve',
+  },
+} as const satisfies AdditionalConfigs
 
 const build = async () => {
   await Promise.all(
-    Object.entries(moduleResolutions).map(
-      async ([, { directory, modules }]) => {
-        await fs.mkdir(path.join(ROOT_DIRECTORY, directory), {
+    [...Object.entries(baseConfigs), ...Object.entries(additionalConfigs)].map(
+      async ([
+        ,
+        { directory, module: moduleKind, moduleResolution, subDirectory },
+      ]) => {
+        const directoryPath = path.join(ROOT_DIRECTORY, directory, subDirectory)
+
+        const tsconfigJsonPath = path.join(directoryPath, 'tsconfig.json')
+
+        const withJsDirectoryPath = path.join(directoryPath, 'with-js')
+
+        const withJsTsConfigJsonPath = path.join(
+          withJsDirectoryPath,
+          'tsconfig.json',
+        )
+
+        await fs.mkdir(directoryPath, {
           recursive: true,
         })
 
-        const moduleEntries = Object.entries(modules)
+        await fs.mkdir(withJsDirectoryPath, {
+          recursive: true,
+        })
 
-        if (moduleEntries.length > 1) {
-          await Promise.all(
-            moduleEntries.map(async ([, moduleName]) => {
-              const dir = await fs.mkdir(
-                path.join(ROOT_DIRECTORY, directory, moduleName, 'with-js'),
-                {
-                  recursive: true,
-                },
-              )
+        const baseTsconfigJson = {
+          $schema: 'https://json.schemastore.org/tsconfig',
+          compilerOptions: {},
+          display: `TypeScript configuration with \`${moduleResolution}\` module resolution`,
+        } as const satisfies TsConfigJson
 
-              if (dir != null) {
-                console.log(`${dir} created`)
-              }
+        const withJsTsConfigJson = {
+          ...baseTsconfigJson,
+          compilerOptions: {
+            allowJs: true,
+            checkJs: true,
+          },
+          display: `TypeScript configuration with module set to \`${moduleKind}\`, module resolution set to \`${moduleResolution}\`, and JavaScript files support enabled`,
+          extends: [packageJson.name, directory, subDirectory]
+            .filter((e) => !!e)
+            .join('/'),
+        } as const satisfies TsConfigJson
+
+        const tsconfigJson = {
+          ...baseTsconfigJson,
+          compilerOptions: {
+            ...(moduleKind !== baseConfigs.node.module && {
+              module: moduleKind,
             }),
-          )
-        }
+            ...(moduleResolution !== 'node' && { moduleResolution }),
+            ...(moduleResolution === 'node' && {
+              allowSyntheticDefaultImports: true,
+              declaration: true,
+              esModuleInterop: true,
+              forceConsistentCasingInFileNames: true,
+              isolatedModules: true,
+              jsx: 'react',
+              lib: ['DOM', 'ESNext'],
+              module: 'esnext',
+              moduleDetection: 'force',
+              moduleResolution,
+              noEmit: false,
+              noEmitOnError: true,
+              noErrorTruncation: true,
+              noFallthroughCasesInSwitch: true,
+              noImplicitOverride: true,
+              noImplicitReturns: true,
+              resolveJsonModule: true,
+              skipLibCheck: true,
+              sourceMap: true,
+              strict: true,
+              target: 'esnext',
+              types: ['node', 'vitest/globals', 'vitest/importMeta'],
+              useDefineForClassFields: true,
+              useUnknownInCatchVariables: true,
+            }),
+          },
+          display: `TypeScript configuration with module set to \`${moduleKind}\` and module resolution set to \`${moduleResolution}\`${moduleResolution === 'node' ? ', intended for TypeScript versions earlier than 5.0.' : moduleResolution === 'node10' ? '. It serves as a backwards compatible replacement for the deprecated `node` module resolution.' : ''}`,
+          ...(moduleResolution !== 'node' && {
+            extends: `${packageJson.name}/node`,
+          }),
+        } as const satisfies TsConfigJson
+
+        const prettierConfig = (await resolveConfig(tsconfigJsonPath)) ?? {}
+
+        fs.writeFile(
+          tsconfigJsonPath,
+          await format(JSON.stringify(tsconfigJson, null, 2), {
+            ...prettierConfig,
+            filepath: tsconfigJsonPath,
+          }),
+          { encoding: 'utf-8' },
+        )
+
+        fs.writeFile(
+          withJsTsConfigJsonPath,
+          await format(JSON.stringify(withJsTsConfigJson, null, 2), {
+            ...prettierConfig,
+            filepath: withJsTsConfigJsonPath,
+          }),
+          { encoding: 'utf-8' },
+        )
       },
     ),
   )
